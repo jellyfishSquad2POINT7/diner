52b05b32755128cc7801100428df2333db93c05b
Jay Coughlan <JaysCogs@gmail.com>
2017-04-07 01:30:28 -0700

    I have completed steps 1-8 for the backend to the best of my ability. I've added many post, put, and delete methos as well as set up the restfu interface. there are likely some bugs, but until frontend is complete, it is hard to test.

--- application/views/template.php
+++ application/views/template.php
@@ -9,10 +9,9 @@
 	</head>
     <body>
 		<div class="container">
-			{navbar}
+        
             {content}
-            <p class="footer">Page rendered in <strong>0.0155</strong> seconds. 
-                {ci_version}</p>
+            
         </div>
         {caboose_scripts}
 		{caboose_trailings}
